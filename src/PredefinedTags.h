3e6cc61e4855c7191ad98ca63f0ba5f3ec939abf
ATATC <futerry@outlook.com>
2024-04-06 23:04:25 -0400

    Updated predefined tags.

--- src/PredefinedTags.h
+++ src/PredefinedTags.h
@@ -7,9 +7,15 @@
 #define CENTER_REAR_WHEEL_SPEED_SENSOR "wss_cr"
 #define LEFT_REAR_WHEEL_SPEED_SENSOR "wss_lr"
 #define RIGHT_REAR_WHEEL_SPEED_SENSOR "wss_rr"
+#define ACCELEROMETER "accl"
+#define ODOMETER "odm"
 #define VOLTAGE_SENSOR "vot"
 #define THROTTLE_PEDAL "tpd"
 #define BRAKE_PEDAL "bpd"
+#define MOTOR_CONTROLLER "mc"
+#define GPS_RECEIVER "gps"
+#define LEFT_INDICATOR "lindi"
+#define RIGHT_INDICATOR "rindi"
 
 
 #endif // PREDEFINEDTAGS_H
