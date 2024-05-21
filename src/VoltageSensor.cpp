c2d573d26187338d900b5aaef2aa08859ae94189
ATATC <futerry@outlook.com>
2024-05-21 16:48:44 -0400

    Fixed voltage sensor algorithm. (#182)

--- src/VoltageSensor.cpp
+++ src/VoltageSensor.cpp
@@ -4,4 +4,4 @@ VoltageSensor::VoltageSensor(int *const pins) : Device<float>(pins) {}
 
 void VoltageSensor::initialize() { pinMode(_pins[0], INPUT); }
 
-float VoltageSensor::read() { return int(((float) analogRead(_pins[0]) / 4.092)) % 100 / 10; }
+float VoltageSensor::read() { return (float) analogRead(_pins[0]) * 5 / 1023; }
