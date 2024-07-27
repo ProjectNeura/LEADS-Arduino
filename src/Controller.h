a616d3847ea596d3479536198a54316c0a199926
ATATC <futerry@outlook.com>
2024-07-27 19:19:15 +0800

    Merged Controller.cpp and Controller.h. (#320)

--- src/Controller.h
+++ src/Controller.h
@@ -4,17 +4,26 @@
 
 #include "Device.h"
 
-template<typename T>
+template<typename T, typename E>
 class Controller : public Device<T> {
 protected:
-    ArrayList<Device<String>> _devices;
-    void _attachDevice(String tag, Device<String> device);
+    ArrayList<String> _device_tags;
+    ArrayList<Device<E>> _devices;
+    void _attachDevice(String &tag, Device<E> device) {
+        _device_tags.add(tag);
+        _devices.add(device);
+    }
 
 public:
-    Controller();
-    int level();
-    void device(String tag, Device<String> device);
-    Device<String> device(String tag);
+    Controller() : Device<T>() {}
+    int level() { return this->_parentTags.size(); }
+    void device(String &tag, Device<E> device) { _attachDevice(tag, device); }
+    Device<E> device(String &tag) { return _devices.get(_device_tags.indexOf(tag)); }
+    virtual void initialize(const ArrayList<String> &parentTags) {
+        Device<E>::initialize(parentTags);
+        for (Device<E> device: _devices)
+            device.initialize(merge(this->_parentTags, parentTags));
+    }
 };
 
 
