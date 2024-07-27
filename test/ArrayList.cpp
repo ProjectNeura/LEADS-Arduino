0d479ba548f60471ea87e78a841b57b46251f4e5
ATATC <futerry@outlook.com>
2024-07-27 19:04:09 +0800

    Added ArrayList test. (#320)

---
+++ test/ArrayList.cpp
@@ -0,0 +1,14 @@
+#include "Arduino.h"
+#include "LEADS.h"
+
+ArrayList<int> test = ArrayList<int>();
+
+void setup() { Serial.begin(9600); }
+
+void loop() {
+    test.add(1);
+    if (test.size() > 20)
+        test.clear();
+    Serial.println(test.size());
+    delay(100);
+}
