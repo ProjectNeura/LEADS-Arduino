577f9a7a829f9eb9c1ca7e5a698c6844f154c613
ATATC <futerry@outlook.com>
2024-07-28 00:14:15 +0800

    Removed unused utils. (#319)

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -2,10 +2,6 @@
 
 bool pulseTriggered(int pin) { return digitalRead(pin) == LOW; }
 
-bool equivalent(float a, float b, float epsilon) { return abs(a - b) <= epsilon * max(abs(a), abs(b)); }
-
-bool equivalent(long a, long b, float epsilon) { return labs(a - b) <= epsilon * max(labs(a), labs(b)); }
-
 void returnFloat(const String &tag, float n) {
     Serial.print(tag + ":");
     Serial.print(n);
--- src/Utils.h
+++ src/Utils.h
@@ -6,10 +6,6 @@
 
 bool pulseTriggered(int pin);
 
-bool equivalent(long a, long b, float epsilon);
-
-bool equivalent(float a, float b, float epsilon);
-
 void returnFloat(const String &tag, float n);
 
 void returnDouble(const String &tag, double n);
