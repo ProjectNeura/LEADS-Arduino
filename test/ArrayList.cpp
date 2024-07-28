8561634711cd9da688c022f91a07d11d317c4bc4
ATATC <futerry@outlook.com>
2024-07-29 00:06:51 +0800

    Updated test ArrayList.cpp. (#321)

--- test/ArrayList.cpp
+++ test/ArrayList.cpp
@@ -1,13 +1,18 @@
 #include "LEADS.h"
 
-ArrayList<int> test = ArrayList<int>();
+const int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+ArrayList<int> b = ArrayList<int>(a, 10);
+const int c[] = {12, 13};
+ArrayList<int> d = ArrayList<int>(c, 2);
 
-void setup() { Serial.begin(9600); }
+void setup() {
+    Serial.begin(9600);
+    b.add(11);
+    b = b + d;
+}
 
 void loop() {
-    test.add(1);
-    if (test.size() > 20)
-        test.clear();
-    Serial.println(test.size());
-    delay(100);
+    Serial.println(b.size());
+    Serial.println(b[11]);
+    delay(1000);
 }
