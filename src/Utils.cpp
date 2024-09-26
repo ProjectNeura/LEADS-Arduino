705289277b61061c3b144157face3dae94833696
ATATC <futerry@outlook.com>
2024-09-26 16:47:39 -0400

    Removed `pulseTriggered()` and added `returnString()`. (#87)

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -1,6 +1,6 @@
 #include "Utils.h"
 
-bool pulseTriggered(int pin) { return digitalRead(pin) == LOW; }
+void returnString(Peer &peer, const String &tag, const String &msg) { peer.write(tag + ":" + msg); }
 
 void returnFloat(Peer &peer, const String &tag, float n) { peer.write(tag + ":" + n); }
 
